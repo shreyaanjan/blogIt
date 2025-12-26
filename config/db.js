18bd61222929114edac610727c6d705cd30a3057
shreyaanjan <shreyaanjan02@gmail.com>
2025-12-26 17:08:04 +0530

    minor error solved

--- config/db.js
+++ config/db.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose"
-const monogo_URI = process.env.monogo_URI
+const MONGO_URI = process.env.MONGO_URI
 
 const connectDb = async () => {
     try {
         mongoose.connection.on('connected', () => {
             console.log("Database connected successfully.");
         })
-        await mongoose.connect(monogo_URI)
+        await mongoose.connect(MONGO_URI)
     } catch (error) {
         console.log(error);
     }
